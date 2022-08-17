9d4f6fcdfccfa5e497431d85eab3ba8a2ff509ea
Dangan <pro.1.h@outlook.com>
2022-08-16 19:55:40 -0600

    time

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-// Change heading:
+// Variables
+var timeDisplayEl = $('#time-display');
+var projectDisplayEl = $('#project-display');
+var projectModalEl = $('#project-modal');
+var projectFormEl = $('#project-form');
+var projectNameInputEl = $('#project-name-input');
+var projectTypeInputEl = $('#project-type-input');
+var hourlyRateInputEl = $('#hourly-rate-input');
+var dueDateInputEl = $('#due-date-input');
+
+// Display Time
+function displayTime() {
+    var rightNow = moment().format('MMM DD, YYYY [at] hh:mm:ss a');
+    timeDisplayEl.text(rightNow);
+}
+
+//Print date to the page
+function printProjectDate(name, type, hourlyRate, dueDate) {
+    var projectRowEl = $('');
+}
