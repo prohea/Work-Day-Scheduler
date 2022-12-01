024becdfebe6004f971ad78d65636d01e6707026
Mizuri <pro.1.h@outlook.com>
2022-11-30 17:13:09 -0700

    added script

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,50 +1,29 @@
-var $timeBlocks = $(".time-block");
-var $scheduleArea = $(".schedule");
-
-var toDoItems = [];
-//Each object has an hour property and a text property
 $("#currentDate").text(moment().format('dddd, MMMM Do YYYY'));
 var currentHour = moment().hour();
 
-
 //When the document loads
 $(document).ready(function () {
+    //Calling function to load data from local storage
 	getLocalStorage();
+    //Calling function to change text area color base on criteria
     setUpTimeBlocks();
 
+    //Saving event into localstorage when click on save
     $(".btn").on("click", function() {
+        //Getting value from description text area for each time block
         var inputData = $(this).siblings(".description").val();
-        var inputTime = $(this).parents(".timeBlocks").attr("id").split("-")[1];
+        //Getting hour value from each time block by id
+        var inputTime = $(this).parents(".time-block").attr("id").split("-")[1];
+        //Saving the data in the local storage
         localStorage.setItem(inputTime, inputData);
     });
 	
 });
 
 
-//If there is no to do set up, let's set up the array of objects
-function initializeSchedule() {
-	//For each time block
-	$timeBlocks.each(function () {
-		var $thisBlock = $(this);
-		var thisBlockHr = parseInt($thisBlock.attr("data-hour"));
-
-		var todoObj = {
-			//Set related todo hour to same as data hour
-			hour: thisBlockHr,
-			//Get text ready to accept string input
-			text: "",
-		};
-		//Add todo object to array
-		toDoItems.push(todoObj);
-	});
-
-	//Loop through timeblocks, save array of objects to local storage by stringifying it first
-	localStorage.setItem("todos", JSON.stringify(toDoItems));
-}
-
 //Format timeblock colors depending on the time
 function setUpTimeBlocks() {
-	$(".timeBlocks").each(function () {
+	$(".time-block").each(function () {
 		var numtextArea = $(this).attr("id").split("-")[1];
 
 		//Add style to time blocks to show where we are in the day
@@ -60,43 +39,15 @@ function setUpTimeBlocks() {
 	});
 }
 
-function renderSchedule() {
-	toDoItems = localStorage.getItem("todos");
-	toDoItems = JSON.parse(toDoItems);
-
-	//Loop through array then assign the text to the timeblock with data hour to hour
-	//Make a variable where then plug it it into the selector
-	for (var i = 0; i < toDoItems.length; i++) {
-		var itemHour = toDoItems[i].hours;
-		var itemText = toDoItems[i].text;
-
-		$("[data-hour=" + itemHour + "]")
-			.children("textarea")
-			.val(itemText);
-	}
-	console.log(toDoItems);
-}
-
-function saveHandler() {
-	var $thisBlock = $(this).parent();
-
-	var hourToUpdate = $(this).parent().attr("data-hour");
-	var itemToAdd = $(this).parent().children("textarea").val();
-
-	//See which item needs to update based on the hour of the button clicked matching
-	for (var j = 0; j < toDoItems.length; j++) {
-		if (toDoItems[j].hour == hourToUpdate) {
-			//Set its text to what was added
-			toDoItems[j].text = itemToAdd;
-		}
-	}
-}
-
+//Function to load all the data from local storage
 function getLocalStorage() {
-    $(".timeBlocks").each(function() {
+    $(".time-block").each(function() {
+        //Getting hour value from each time block by id
         var numtextArea = $(this).attr("id").split("-")[1];
+        //Getting date from local storage for each text area
         var getData = localStorage.getItem(numtextArea);
         console.log(numtextArea, getData);
+        //Inserting data into the text area for each element
         $(this).children(".description").val(getData);
     });
 }
